40b741b11c3ac05ea18cf4f79fdf51675da67366
reginaldo-basilio <reginaldobasiliocunha@gmail.com>
2023-04-30 00:40:39 -0300

    Criando layouts, estilo e estruturando o projeto

---
+++ Gestor_Tarefas/resources/views/layouts/main.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/app.css">
+        
+    </head>
+    <body>
+        <header>
+           <nav class = "navbar navbar-expand-lg navbar-light">
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class="nav-link">Novo usuário</a>
+                    </li>
+                </ul>
+            </div>
+           </nav> 
+        </header>
+    @yield('content')    
+
+    <footer>
+        <p>Gestor Tarefas &copy; 2023</p>
+    </footer>
+    </body>
+</html>
